559156c863ad51d5fd127a99074f0990cb51bc7f
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-07-27 18:16:57 +0000

    variables configure files

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 145612
+let accountEmail = "manish@google.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountstate;
+
+
+//account = 3
+console.log(accountId);
+
+accountEmail = "golu@google.com"
+accountPassword = "11235"
+accountCity = "Punjab"
+
+
+console.table([accountId ,accountEmail, accountPassword, accountCity ,accountstate])
