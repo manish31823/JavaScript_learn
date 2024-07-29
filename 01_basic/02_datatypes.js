c517ba38dfc859852c51a6402b010625cbfba66d
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-07-29 06:10:46 +0000

    datatypes js

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // This line treat as the newer version in javascript 
+// It is not complusory to write this but got to go with the synaax and its flows
+
+
+
+// alert(3+3)// we are using node js , not the web browser.
+
+
+//  console.log(3+3);
+  console.log("Manish")
+
+// Datatypes in js
+let name = "manish"
+let age = 23
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// bigint => used in _trading,stock market
+// string => ""
+// boolean => true/false
+// null => Standalone  value
+// undefined => value not assign yet
+// symbol => unique
+// These are the primitive datatypes in js
+
+//object
+
+
+console.log(typeof null); // NUll is an object
+console.log(typeof undefined); // NUll is an object
