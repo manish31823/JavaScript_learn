0e074cad9ec0350ce15220fba33d223ed8525dde
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-07-31 14:30:29 +0000

    Conversion operations

--- 01_basic/03_conversionoperation.js
+++ 01_basic/03_conversionoperation.js
@@ -2,13 +2,13 @@
 //let score = "null"//
 let score = null
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 // "33" => 33
 // "33abc" => NaN
 //  true => 1; false => 0
@@ -16,7 +16,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = Boolean
 let BooleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -27,3 +27,66 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+
+
+
+/*-----------------------operations--------------------*/
+
+let value  = 5
+let negValue = -value
+
+console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(3*3);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 =  " Manish"
+console.log(str1 + str2);
+
+
+
+
+console.log(true);
+console.log(+true);
+console.log(-true);
+console.log("");
+
+
+let GameCounter = 200
+GameCounter--;
+console.log(GameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
