f9d786c67f9af40b71723dc663270d5caa934420
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-01 09:33:57 +0000

    Comparision Operations

---
+++ 01_basic/04_Comparision.js
@@ -0,0 +1,42 @@
+//Basic----------------------*/
+console.log(2 > 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 !=1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+
+
+// AVoid these tyes oF code started 
+console.log(null > 0);
+console.log(null == 0);
+console.log(null != 0);// It will treat the null value = 0 so it will be true
+
+let undefinded = 0
+console.log(undefinded > 0);
+console.log(undefinded == 0);/// Comaprsion and eauality check are two diffrent thinks in js
+console.log(undefinded < 0);
+Ended
+
+//Strict check in js(===)
+console.log("2" == 2);//conversion staared here
+console.log("2" === 2);// It will check the datatype whether it is string or number.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
