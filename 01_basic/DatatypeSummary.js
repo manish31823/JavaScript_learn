26e12c9bd13d2ac741d6fe4522b9642900f4c237
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-01 11:24:22 +0000

    Datatype summary

---
+++ 01_basic/DatatypeSummary.js
@@ -0,0 +1,51 @@
+//DataTypes In js (Also Called Call By value.)
+
+// Primitive Datatype(7) - String , Number , Null , Undefined , Bigint , Boolean , Symbol
+
+// const score: = 100 //assign the value in TypeScript
+
+const score = 100 //number
+const scoreValue = 100.3355
+const isLoggedIn = false// Boolean 
+
+// const temp = null //assign the null value completely empty
+
+// let email; //It is an undefined value 
+// const person = "manish" // String
+
+// // // AsSign the symbol in js
+ const id = Symbol('123')
+ const anotherid = Symbol('123')
+console.log(id===anotherid);
+
+// // //Big int
+// // const bignumber = 254644556454554n
+
+// // //Non Primitive Datatype = Call By Refrence
+// // //Array , Objects , Functions
+
+
+// //Assign the array
+const crickter = ["Dhoni","Kolhi","ROhit","Jadeja"]
+console.log(crickter);
+
+//Assign the objcts in js
+let myobj = {
+    name : "manish",   // It is written in key value of pairs //
+    age :   23,
+    salary : 100000,
+    city : "delhi",
+  
+
+// };
+// console.log(myobj);
+
+
+//Assign the Function
+// const myfunction = function(){
+//     console.log("Hello MAnish");
+
+};
+
+// know the types of datatype
+// console.log(typeof myfunction);
