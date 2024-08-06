61b6d37ef21347fb6a5269156d16a6ed8c537849
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-06 16:20:21 +0000

    dates

--- 01_basic/07_dates.js
+++ 01_basic/07_dates.js
@@ -3,6 +3,8 @@
 let myDate = new Date()
 console.log(myDate);
 console.log(myDate.toDateString());
-console.log(myDate.toISOString());
-console.log(myDate.toJSON());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
 
