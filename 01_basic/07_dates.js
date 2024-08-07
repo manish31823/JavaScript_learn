238ca1bad14a0759dae8d132e5adc80f1ab061ad
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-07 15:11:15 +0000

    dates -time

--- 01_basic/07_dates.js
+++ 01_basic/07_dates.js
@@ -1,10 +1,33 @@
 // Dates in js
  
-let myDate = new Date()
+let myDate = new Date()//object creation
 console.log(myDate);
 console.log(myDate.toDateString());
-// console.log(myDate.toISOString());
-// console.log(myDate.toJSON());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
 console.log(myDate.toLocaleDateString());
 console.log(myDate.toLocaleString());
+
+let mycreatedDate = new Date ("8-7-2024")
+// console.log(mycreatedDate.toLocaleString());
+
+
+// //Timestamps in js
+// // for design quiz ,polls
+ let myTimeStamp =  Date.now()
+// //  console.log(myTimeStamp);
+//  console.log(mycreatedDate.getTime());
+//  console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+console.log(newDate.getHours());
+
+// imp----- what is locale string 
+// it is used for customation of date,timezone, everything
+
+ 
+ 
+ 
 
