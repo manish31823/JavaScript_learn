c94cec2f5ba4bb717d613dc5bdd840cdacf42fc3
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-08 17:04:10 +0000

    js practice

---
+++ 01_basic/practice.js
@@ -0,0 +1,12 @@
+//SIngle line comment
+// Any text between // and the end of the line will be ignored by JavaScript (will not be executed).
+
+// This example uses a single-line comment before each code line:
+
+/* Muti line comment 
+
+-----------------------------------------------
+
+
+
+---------------------------------------------------*/
