4a3cbbefdd68fae4130359f611ba41f8490f0c90
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-09 07:45:32 +0000

    array-2

---
+++ 02_basic/02_array.js
@@ -0,0 +1,5 @@
+const ipl_players = ["Rusell", "Rashid", "Conway", "Bravo"]
+const indian_players = ["Dhoni", "Rohit","Kolhi", "Bumrah"]
+
+ipl_players.push(indian_players)
+console.log(ipl_players);
