0a6fa39a89970d80ddb538339e966c7fd56a4b38
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-09 17:18:20 +0000

    Array

--- 02_basic/02_array.js
+++ 02_basic/02_array.js
@@ -3,3 +3,26 @@ const indian_players = ["Dhoni", "Rohit","Kolhi", "Bumrah"]
 
 ipl_players.push(indian_players)
 console.log(ipl_players);
+
+//Spread operator..... .....>
+const momos = ["cho","kho","lho"]
+const paneer = ["choi","khoi","lhoii"]
+const allitem = [...momos, ...paneer] // This is spread operator or we can use concatenate 
+// console.log(allitem);
+
+//Intersting array
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+const new_another_array = another_array.flat(Infinity)
+console.log(new_another_array);// by the help of flat we can modify any unique sort of an array
+
+//Data scraping of web page -node list ,array,string
+console.log(Array.isArray("Manish"))
+console.log(Array.from("Manish"))//convert
+console.log(Array.from({name : "Manish"})) //Intersting array it will give empty array beacuse does kniw the type of array of key or values
+
+// Array of score
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
+
