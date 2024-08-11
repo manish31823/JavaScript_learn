62e5fcc3b4130c9f3028b880864a8a7468a4dab1
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-11 15:10:23 +0000

    object

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,6 @@
+// How to use the objects in single ton using constructor 
+
+// const Appleuser = new object ()  // This is a singleton
+const Appleuser = {} // this is just an object
+ console.log(Appleuser);
+ 
