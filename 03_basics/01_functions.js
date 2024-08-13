2cf54a534c1374851365cb9b07ab1260b82810bb
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-13 14:56:06 +0000

    functions and parameters

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,16 @@
+// // creating a function
+function Hello (){
+    console.log("hello Function");
+    
+}_
+// _________________________________.........> basic structure of creating an function
+Hello()
+
+
+//add of two number 
+
+function add (number1 ,number2){//this is parameter
+   console.log( number1+number2);
+    
+}
+add(13,null)// argument
