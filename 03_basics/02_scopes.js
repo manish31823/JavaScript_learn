064605b9ed1dccb7e07e6dfe543d4e338ccb1ae7
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-15 06:04:47 +0000

    js scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,19 @@
+// Global and local scope in js
+// scope keyword
+let a = 10
+const b = 20 ///global scope
+var c = 30
+
+
+if (true) {
+    let a = 100
+    const b = 200 //local scope or block scope
+    var c = 300 // var always delclare its inner scope not to use var mostly avoid of block scope
+    console.log("Inner :",a); //if we print here it will print inner 100 and outside 10
+    
+}
+
+
+console.log(a)
+// console.log(b)
+// console.log(c)
