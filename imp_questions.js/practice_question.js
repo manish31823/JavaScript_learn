aac14fe40ef1454b2e2d10a93cbbda143fadd1b8
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-15 15:48:58 +0000

    practice-js

---
+++ imp_questions.js/practice_question.js
@@ -0,0 +1,19 @@
+// 1// define an array in js
+// const array = [20,30,40,50]
+// console.log(array);
+
+
+
+// creting an object in js
+const salesuser = {
+    username : "golu",
+    place : "new delhi",
+    age : 22,
+    salary : 10000
+}
+console.log(salesuser);
+
+
+// creating an string in js
+const name = "Manish"
+console.log(typeof "name");
