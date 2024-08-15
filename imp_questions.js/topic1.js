47c16dec2cf4961f7c07af3068eee658687cb1a7
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-15 10:09:39 +0000

    question js

---
+++ imp_questions.js/topic1.js
@@ -0,0 +1,48 @@
+// 1. what is js? write its synatx ?
+// 2. Define variable and its types.
+// js datatypes and what are ECMA standard
+// 4 Datatypes coversion----- operation all data types
+// Q why the string to number is confusion 
+// js is a ststically type or dynamically type of language
+//  what is blcok level scope
+// comparsion of datatypes
+// types of dataypes in js
+// what are stack and heap memory in js?
+// what is string interpolation 
+// what is strings in js
+// number and maths in js
+// dtae and time in js
+// what are array in js and its is non primtive?
+// what are shallow copy(stack) and deep copy refrences
+// diff b/w slice and splice method in js
+// what is spread and rest operator and what is concatation
+// how to manage the arry into arrat into array like------([2,4,6],[54,65,35],[5,6,8])
+// what is the use of arry in js
+// how to declare the string
+// how to declare the array
+// how to declare the objects
+// how to declare the array
+// how to declare the function
+// ways to declare the objecs -----literals and constructor
+// what is single ton(constructor)
+// what is object literals
+// what are key and values in objects
+// method of string and obects
+// object value acess 
+// how to chnage object value
+// string method impppp
+// how we can freeeze the object 
+// what is destructure of object
+// what is json 
+// what is api ?why is json over xml
+// what is function and ist parameter
+// define function and is syntax
+// function of add two number
+// work of return value in function 
+// create a function login user messagw
+// function with objetc and array
+// calacate thr price of shoppoing price
+// how to pass multiple vaues into function
+// makinf and combine of arraya and objrct in the function
+// what is scope and what are global and local scope
+
