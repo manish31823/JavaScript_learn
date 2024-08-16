83189d24b9f42e8614393e7c6b45572c38b00910
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-16 14:23:24 +0000

    scope level

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,11 +9,30 @@ if (true) {
     let a = 100
     const b = 200 //local scope or block scope
     var c = 300 // var always delclare its inner scope not to use var mostly avoid of block scope
-    console.log("Inner :",a); //if we print here it will print inner 100 and outside 10
+    // console.log("Inner :",a); //if we print here it will print inner 100 and outside 10
     
 }
 
 
-console.log(a)
+// console.log(a)
 // console.log(b)
 // console.log(c)
+ 
+
+// what is nested scope?
+
+function one(){
+   const username = "Manish"
+    
+
+    function two(){
+       const website = "youtube" //kind of closure of nested function
+       console.log(username);
+//function declaration to make an another call stack       
+    }
+    // console.log(website); //throw an refrence error
+
+    two()
+    
+}
+one()
