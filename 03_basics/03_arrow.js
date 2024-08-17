323ae3b2512178402532f3b2a5bd44acc931d30f
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-17 16:35:04 +0000

    arrow and this

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -17,4 +17,45 @@ const user = {
 // user.welcomeMessage()
 // user.username = "golu" //value has no hardcore here
 // user.welcomeMessage()
-console.log(this);
+// console.log(this);
+
+
+// function created
+
+// function Momos(){
+//     let username = "Mritunjay"
+//     console.log(this.username);
+    
+
+// }
+
+// Momos()
+
+const Momos = function(){
+    let username = "Mritunjay"
+        console.log(this.username);
+
+}
+
+
+// assign an arrow function just removed the function keyword and put the =>
+
+const Momos2 = () => {
+    let username = "Mritunjay"
+        console.log(this.username);
+
+}
+// Momos()
+
+// assigan an simple Arrow function or explicit return 
+// const addnumber = (num1,num2) => {
+//     return num1+ num2
+
+// }
+// console.log(addnumber(50,40));
+
+// implict return removes the parenthesis form function
+const addnumber = (num1,num2) =>   num1 + num2
+
+
+console.log(addnumber(50,40));
