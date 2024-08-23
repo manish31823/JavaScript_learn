35c6095356988f22365aa44aae59a3d92d056706
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-23 14:31:08 +0000

    controlflow

---
+++ 04_Control_flow.js/1.js
@@ -0,0 +1,12 @@
+// if statement 
+// < , > , <= ,>= , == , !=, !== , ===
+
+const isLoggedIn = true
+const temp = 58
+if (temp < 50) {
+    console.log("temperature is good");
+    
+}else{
+    console.log("temperature is hot");
+    
+}
