1fa305dd16214996d0ba77c8dca34915c9f7ab1a
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-23 15:20:18 +0000

    flow

--- 04_Control_flow.js/1.js
+++ 04_Control_flow.js/1.js
@@ -10,3 +10,48 @@ if (temp < 50) {
     console.log("temperature is hot");
     
 }
+
+const score = 200
+if (score > 100) {
+    // beacuse of a global scope we can define global scope of the variable
+  let power = "fly"
+    console.log(`use power : ${power}`);
+    
+    
+}
+console.log(`use power : ${power}`);
+
+// shorthand notation(implict scope)
+const balance = 5000
+// if (balance>2000)    console.log("passed");
+if (balance < 3000) {
+    console.log("less than 3000");
+    
+    
+}else if(balance< 3500){
+    console.log("less than 3500");
+    
+}else if(balance< 4500){
+    console.log("less than 4500");
+    
+}else{
+    console.log("less than 6000");
+    
+
+// }
+    
+ // real life example checking of the multiple statement
+const isloggedin = true
+const debitcard = true
+const isloogedFromGoogle = true
+const isloogedFromGmail = true
+if (isloggedin && debitcard) {
+    console.log("Allow to shopping");
+    
+    
+}
+
+if (isloogedFromGoogle  || isloogedFromGmail) {
+    // console.log("user logged in");
+    
+}
