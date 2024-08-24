a2f63f8b2d1fdbf8f402008e06fcebd0fecae411
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-24 15:05:50 +0000

    loop

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -14,15 +14,57 @@
 
 
 // loop * loop and printing table
-for (let i = 1; i <= 10; i++) {
-    console.log(`Outer loop value ${i}`);
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`Outer loop value ${i}`);
     
-    for (let j = 0; j <= 10; j++) {
-//    console.log(`Inner loop value ${j} and inner loop is ${i}`);
-    console.log(i + '*' + j + '=' + i*j);
+//     for (let j = 0; j <= 10; j++) {
+// //    console.log(`Inner loop value ${j} and inner loop is ${i}`);
+//     console.log(i + '*' + j + '=' + i*j);
     
    
         
+//     }
+    
+// }
+
+
+// for declaring an array in the loop
+let myarray = ["dhoni" , "kolhi" , "rohit" , "jadeja"]
+console.log(myarray.length);
+
+for (let index = 0; index < myarray.length; index++) {
+    const element = myarray[index];
+    console.log(element);
+    
+    
+}
+
+
+// break and contiue statement
+// break
+for (let i = 1; i <=20; i++) { //here we can detect or check in any number with the break keyword
+    if (i==6) {
+        console.log("number Detected 6");
+        break
+        
+        
     }
+  console.log(`value of the i : ${i}`);
+  
+    
+}
+
+
+
+// continue statement
+for (let i = 1; i <=20; i++) { //here we can detect or check in any number with the break keyword
+    if (i==6) {
+        console.log("number Detected 6");
+        continue
+        
+        
+    }
+  console.log(`value of the i : ${i}`);
+  
     
 }
