317e339c04b70031d89c3e0af0335fbe11da131f
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-24 14:14:55 +0000

    loop

---
+++ 05_iteration/one.js
@@ -0,0 +1,13 @@
+// for loop
+
+for (let j = 0; j <=10; j++) {
+    const element = j; // loop and if condtional
+    if (element==6) {
+        console.log("7 is the best number");
+        
+        
+    }
+    console.log(element);
+    
+}
+// console.log(element);
