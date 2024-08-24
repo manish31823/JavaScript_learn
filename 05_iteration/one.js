f65d0d84448214bfc97c161ba4399336adf54025
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-24 14:34:55 +0000

    loop

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -1,13 +1,28 @@
 // for loop
 
-for (let j = 0; j <=10; j++) {
-    const element = j; // loop and if condtional
-    if (element==6) {
-        console.log("7 is the best number");
+// for (let j = 0; j <=10; j++) {
+//     const element = j; // loop and if condtional
+//     if (element==6) {
+//         console.log("7 is the best number");
         
+        
+//     }
+//     console.log(element);
+    
+// }
+// // console.log(element);
+
+
+// loop * loop and printing table
+for (let i = 1; i <= 10; i++) {
+    console.log(`Outer loop value ${i}`);
+    
+    for (let j = 0; j <= 10; j++) {
+//    console.log(`Inner loop value ${j} and inner loop is ${i}`);
+    console.log(i + '*' + j + '=' + i*j);
+    
+   
         
     }
-    console.log(element);
     
 }
-// console.log(element);
