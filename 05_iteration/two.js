d8f734dc6d597155e02d4cb867dae08350dede6f
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-25 07:59:11 +0000

    do loop

--- 05_iteration/two.js
+++ 05_iteration/two.js
@@ -1,10 +1,32 @@
-// while and do while loop
+// // while and do while loop
 
-// while loop
-let index = 0
-while (index <=10) {
-   console.log(`value of index is ${index}`);
-   index = index + 2
-     
+// // while loop
+// let index = 0
+// while (index <=10) {
+//    console.log(`value of index is ${index}`);
+//    index = index + 2
+      
+// }
+
+
+// while loop in array
+let myarray = ['rohit' , "mohit" , "sohit" ]
+let array = 0  // varaiable declaration
+while ( array <= myarray.length) {
+    console.log(`value  is ${myarray[array]}`);
+   array = array+1
+    
     
 }
+
+
+
+// do while loop 
+// here value will be 18 beacsue here work id done first then condition will checked
+let score = 18
+do {
+    console.log(`score is ${score}`);
+    score ++
+    
+    
+} while (score<15);
