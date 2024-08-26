2fea3796ab08c8d2790f041a8842b98d54baa2e0
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-26 08:28:32 +0000

    loop

---
+++ 05_iteration/three.js
@@ -0,0 +1,20 @@
+// for of loop in js
+
+// const arr = [1,2,3,4,5]
+// for (const num of arr) {
+//     console.log(num);
+    
+    
+// }
+
+// const greeting = "Hello,World!" //greeting is an object 
+// for (const greet of greeting) { // greet is iterator
+//     console.log(`value of each word is ${greet}`);
+//     if (greeting== "r") {
+//         break;
+        
+//     }
+    
+    
+// }
+// console.log("loop exit");
