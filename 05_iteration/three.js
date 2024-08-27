c4e13e83b8cea72f897dff5299dddae1484dbb7a
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-27 06:35:01 +0000

    Map

--- 05_iteration/three.js
+++ 05_iteration/three.js
@@ -18,3 +18,40 @@
     
 // }
 // console.log("loop exit");
+
+
+// map in js it is also an object that hold key-value pair
+
+const map = new Map()
+map.set('IN' , "India")
+map.set('USA' , "United States Of America")
+map.set('Arg' , "Argetina")
+map.set('USA' , "United States Of America") //no duplicate values allowed in the map all the values are uniques
+
+
+// console.log(map);
+
+for (const [key,value] of map) {
+    // console.log(key , ':-',value);
+    
+    
+}
+
+
+// for of loop in the object
+
+const obje = {
+    "game1" : "bgmi",
+    "game2" : "pubg",
+    "game3" : "Nfs",
+}
+console.log(obje);
+
+
+for (const [key,value] of obje) {
+    console.log(key, ':-', value);
+    
+    
+}
+
+// map is iterable but not the object
