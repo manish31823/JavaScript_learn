db0cf215c65eaddf637bf5e9c275184c13a017ab
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-08-30 13:54:59 +0000

    filter

---
+++ 05_iteration/six.js
@@ -0,0 +1,8 @@
+const code = ["js","py","c++","java","django"]
+
+ const values = code.forEach( (items) =>{
+    // console.log(items);
+    return items
+})
+console.log(values);//returned in undefined 
+
