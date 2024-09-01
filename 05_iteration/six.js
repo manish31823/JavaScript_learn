f968070a5821528678a87b0b32018325ccd8bce4
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-09-01 07:59:22 +0000

    filter

--- 05_iteration/six.js
+++ 05_iteration/six.js
@@ -1,8 +1,27 @@
-const code = ["js","py","c++","java","django"]
+// const code = ["js","py","c++","java","django"]
 
- const values = code.forEach( (items) =>{
-    // console.log(items);
-    return items
+//  const values = code.forEach( (items) =>{
+//     // console.log(items);
+//     return items
+// })
+// console.log(values);//returned in undefined 
+
+// js filter method is given call back fyunction then given an condition
+
+// const mynumber = [1,2,3,4,5,6,7,8,9,10]
+
+//  const newnum = mynumber.filter( (nums)=> nums >4)
+// console.log(newnum);
+
+
+// for each
+const number  = [1,2,3,4,5,6,7,8,9,10]
+const numbers = []
+number.forEach((nums)=> {
+    if (nums>5 ) {
+        numbers.push(nums)
+        
+    }
+
 })
-console.log(values);//returned in undefined 
-
+console.log(numbers);
