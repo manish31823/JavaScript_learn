38fff202e41df308b5a1ce16b45554fbcaf3b30a
Manish Chaudhary <128781985+manish31823@users.noreply.github.com>
2024-09-03 07:31:17 +0000

    reduce

--- 05_iteration/Eight.js
+++ 05_iteration/Eight.js
@@ -1,13 +1,43 @@
 // Reduce function in js
 
 //accumulator ,current value ,initial values
-
+// we have give value to accumulator 
 const array1 = [1,2,3]
 
- const total = array1.reduce(function ( acc ,cv ) {
-    console.log(`$ accumulator :{acc} and Current value : ${cv}`);
+//  const total = array1.reduce(function ( acc ,cv ) {
+//     console.log(`$ accumulator :{acc} and Current value : ${cv}`);
     
-    return acc + cv
- } ,0) // here 0 is the accumulator
- console.log(total);
+//     return acc + cv
+//  } ,0) // here 0 is the accumulator
+//  console.log(total);
  
+
+// const mytotal = array1.reduce(  (acc , cv )=> acc +cv ,0)
+// console.log(mytotal);
+
+
+
+//shopping card example in reduce
+
+
+const shoppingcart = [
+   {
+      itemname : "js course",
+      price : 3999
+   },
+   {
+      itemname : "py course",
+      price : 999
+   },
+   {
+      itemname : "java course",
+      price : 5999
+   },
+   {
+      itemname : "full stack course",
+      price : 19999
+   }
+]
+
+ const payment = shoppingcart.reduce( (acc , item) => acc + item.price ,0)
+console.log(payment);
